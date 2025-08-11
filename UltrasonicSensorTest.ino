4241d44cd58c5fc447356f4d7b4e62c2b4d30b87
YoshiKing422 <soni.yash@icloud.com>
2025-08-11 09:55:42 -0400

    Add files via upload

--- UltrasonicSensorTest.ino
+++ UltrasonicSensorTest.ino
@@ -4,7 +4,9 @@
 // Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
 #define TRIGGER_PIN 9
 #define ECHO_PIN 10
-#define LED_PIN 8  // Define LED pin
+#define Danger 8  // Define LED pin
+#define Close 7
+#define Clear 4
 
 // Maximum distance we want to ping for (in centimeters).
 #define MAX_DISTANCE 400
@@ -14,7 +16,9 @@ NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);  // Set pin 8 as output
+  pinMode(Danger, OUTPUT);  // Set pin 8 as output
+  pinMode(Close, OUTPUT);
+  pinMode(Clear, OUTPUT);
 }
 
 void loop() {
@@ -27,9 +31,21 @@ void loop() {
 
   // Turn LED on if distance is 100 cm or less, otherwise off
   if (distance > 0 && distance <= 100) {
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(Danger, HIGH);
   } else {
-    digitalWrite(LED_PIN, LOW);
+    digitalWrite(Danger, LOW);
+  }
+  if (distance > 100 && distance <=185) {
+    digitalWrite(Close, HIGH);
+  }
+  else {
+    digitalWrite(Close, LOW);
+  }
+  if (distance > 185){
+    digitalWrite(Clear, HIGH);
+  }
+  else {
+    digitalWrite(Clear, LOW);
   }
 
   delay(500);
